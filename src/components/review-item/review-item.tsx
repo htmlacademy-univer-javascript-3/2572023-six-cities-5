b1edac7dc7d3a2c492a10a4dde45716e0613a4fc
hempi <115639626+VladislavBukirev@users.noreply.github.com>
2024-12-08 19:06:05 +0500

    try fix conflicts

--- src/components/review-item/review-item.tsx
+++ src/components/review-item/review-item.tsx
@@ -17,7 +17,7 @@ function ReviewItem({ review }: ReviewItemProps): JSX.Element {
     <li className="reviews__item">
       <div className="reviews__user user">
         <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src={review.user.avatarUrl} width="54" height="54" alt="User avatar"/>
+          <img className="reviews__avatar user__avatar" src={user.avatarUrl} width="54" height="54" alt="User avatar"/>
         </div>
         <span className="reviews__user-name">{user.name}</span>
         {user.isPro && <span className="offer__user-status">Pro</span>}
@@ -25,7 +25,7 @@ function ReviewItem({ review }: ReviewItemProps): JSX.Element {
       <div className="reviews__info">
         <div className="reviews__rating rating">
           <div className="reviews__stars rating__stars">
-            <span style={{width: `calc(100% / 5 * ${rating})`}}></span>
+            <span style={{ width: `${(rating / 5) * 100}%` }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
